5bdb6ced2d5fd5f1ec8f42eb1b76160b644c7db6
Amol Umrale <babaiscool@gmail.com>
2016-09-14 18:01:00 +0530

    Removed html parsing function as it is not used anymore.

--- pypi/pypi.sh
+++ pypi/pypi.sh
@@ -5,14 +5,6 @@ do_parallel=true
 list=(vlc-ctrl redcmd redlib wallp imagebot giraf)
 pypi_base_url=https://pypi.python.org/pypi
 
-
-parse_html() {
-	html=$(</dev/stdin)
-	c=$(echo "$html" | grep -Po "<span>\d+</span> downloads in the last day" | grep -Po "\d+")
-	v=$(echo "$html" | tr -d "\n" | grep -Po "<div id=\"breadcrumb\">.*?</div>" | grep -Po "<a.*?>.*?</a>" | sed '3q;d' | sed -rn 's/<a.*>(.*)<\/a>/\1/p')
-	printf "%-15s %-10s %6d\n" $1 $v $c
-}
-
 parse_json() {
 	json=$(</dev/stdin)
 
@@ -25,7 +17,6 @@ parse_json() {
 	printf "%-15s %-10s %6d %6d %6d\n" $1 $v $m $w $d
 }
 
-export -f parse_html
 export -f parse_json
 
 printf "%-15s %-10s %6s %6s %6s\n" package version month week day
