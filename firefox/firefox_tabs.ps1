748c61b32a6d2ba436c9aca7e087cc0ae1e482c0
amol9 <babaiscool@gmail.com>
2017-01-25 17:33:37 +0530

    First commit
    Lists tabs for each of the open Firefox windows.

---
+++ firefox/firefox_tabs.ps1
@@ -0,0 +1,53 @@
+.bluelib.ps1
+
+function Firefox-Tabs() {
+	[CmdletBinding()]
+	param (
+	[string]$url	= ".*",
+	[string]$title	= ".*",
+	[switch]$blank
+	)
+
+	PROCESS{
+		function Main() {
+			Write-Host `n
+			$rjs = Get-Recovery-JS
+			Get-Tabs $rjs
+		}
+		
+		function Get-Recovery-JS () {
+			$profile_path = Get-ChildItem "$env:APPDATA\Mozilla\Firefox\Profiles" -Filter '*.default' | %{$_.FullName}
+			
+			Get-Content $profile_path\sessionstore-backups\recovery.js
+		}
+		
+		function Get-Tabs($rjs) {
+			$rjson = $rjs | ConvertFrom-Json
+			
+			$wc = 0
+			ForEach ($w in $rjson.windows) {
+				$wc++
+				Write-Host "Window: $wc"
+				
+				$tl = New-Object System.Collections.ArrayList
+				
+				ForEach ($t in $w.tabs) {
+					$e = $t.entries[-1]
+					
+					if (Filter-Tab $e.title $e.url) {
+						$o = Make-Object([ordered]@{"Title" = $e.title; "Url" = $e.url})
+						[void]$tl.Add($o)
+					}
+				}
+				Write-Object($tl)
+			}
+		}
+		
+		function Filter-Tab ($t, $u) {
+			$f = [regex]::Match($t, "(?i)$title").Success -and [regex]::Match($u, "(?i)$url").Success
+			$b = ! (!$blank -and [regex]::Match($url, "about:.*").Success)
+			$f -and $b
+		}
+		Main
+	}
+}
