91d5e92597b502051038fdad4c13139b074ee77d
amol9 <babaiscool@gmail.com>
2017-01-26 14:41:04 +0530

    Added rudimentary caching facility. It caches the last used url.

--- reddit/get_reddit_score.ps1
+++ reddit/get_reddit_score.ps1
@@ -6,17 +6,44 @@ function Get-Reddit-Score() {
 	)
 
 	PROCESS{
-		$r = Invoke-WebRequest -Uri $url
+		$cache_filepath = "$PSScriptRoot\grs.txt"
 		
-		$score = [regex]::match($r.Content, "<div\s+class=.score likes.*?>(.*?)</div>").Groups[1].Value
-		$title = [regex]::match($r.Content, "<title>(.*?)</title>").Groups[1].Value
-		$comments = [regex]::match($r.Content, "<div class=.commentarea.*?<span\s+class=.title.*?>(.*?)</span>").Groups[1].Value
+		function Main() {
+			if (! $url) {
+				$url = Get-Cached-Url
+				if (! $url) {
+					Write-Host "no url provided, no cached url found either, please provide a url"
+					Break
+				} else {
+					Write-Host "using cached url: $url"
+				}
+			}
+			
+			$r = Invoke-WebRequest -Uri $url
+			
+			$score = [regex]::match($r.Content, "<div\s+class=.score likes.*?>(.*?)</div>").Groups[1].Value
+			$title = [regex]::match($r.Content, "<title>(.*?)</title>").Groups[1].Value
+			$comments = [regex]::match($r.Content, "<div class=.commentarea.*?<span\s+class=.title.*?>(.*?)</span>").Groups[1].Value
 
-		$out = New-Object PSObject
-		$out | Add-Member NoteProperty Title($title)
-		$out | Add-Member NoteProperty Score($score)
-		$out | Add-Member NoteProperty Comments($comments)
+			$out = New-Object PSObject
+			$out | Add-Member NoteProperty Title($title)
+			$out | Add-Member NoteProperty Score($score)
+			$out | Add-Member NoteProperty Comments($comments)
+			
+			Write-Output $out | Format-List
+			
+			Cache-Url $url
+		}
 		
-		Write-Output $out | Format-List
+		function Cache-Url($url) {	
+			$url | Set-Content $cache_filepath
+		}
+		
+		function Get-Cached-Url() {
+			if (Test-Path $cache_filepath) {
+				Get-Content $cache_filepath
+			}
+		}
+		Main
 	}
 }
